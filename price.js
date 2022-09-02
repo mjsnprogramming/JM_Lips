0fd23f381275419adc6a587dd7a3071e0c972e9b
Majson <majsonlbn@gmail.com>
2022-09-02 18:33:25 +0200

    drop navigation bar

--- price.js
+++ price.js
@@ -5,7 +5,7 @@ new Vue ({
     dropNav: () => {
       const drop_nav = document.querySelector('#nav');
       drop_nav.addEventListener('click', () => {
-        drop_nav.classList.toggle('active');
+        drop_nav.classList.toggle('');
       });
     }
   }
